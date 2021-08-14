8f4801375ad75f859e3e41ebd9697341476d391e
Nick Oelsner <noelsner@gmail.com>
2021-08-14 12:27:32 -0700

    add functionality to register new user, break out login and register components

--- src/App.js
+++ src/App.js
@@ -1,57 +1,89 @@
 import { useState } from 'react'
 import axios from 'axios'
+import Login from './components/Login'
+import Register from './components/Register'
+
+const url = 'https://mondo-robot-art-api.herokuapp.com'
 
 function App() {
   const [bearerToken, setBearerToken] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
+  const [auth, setAuth] = useState(null)
+  const [loginError, setLoginError] = useState('')
+  const [registerError, setRegisterError] = useState('')
 
-  const getToken = async (email, password) => {
-    const credentials = {
-      email: email,
-      password: password,
+  console.log('bearerToken :>> ', bearerToken)
+  console.log('auth :>> ', auth)
+
+  const headers = (token) => ({
+    headers: {
+      'Content-Type': 'application/json',
+      'x-robot-art-api-key': process.env.REACT_APP_API_KEY,
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const login = async (credentials) => {
+    const headers = {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-robot-art-api-key': process.env.REACT_APP_API_KEY,
+      },
     }
-    axios
-      .post('https://mondo-robot-art-api.herokuapp.com/auth/session', credentials, {
-        headers: {
-          'Content-Type': 'application/json',
-          'x-robot-art-api-key': process.env.REACT_APP_API_KEY,
-        },
-      })
+    await axios
+      .post(`${url}/auth/session`, credentials, headers)
       .then((response) => {
-        setBearerToken(response.data.token)
+        const token = response.data.token
+        setBearerToken(token)
+        return token
+      })
+      .then((token) => {
+        exchangeTokenForAuth(token)
       })
       .catch((error) => {
-        setError(error.response.statusText)
+        setLoginError(error.response.statusText)
       })
   }
 
-  const onSubmit = (e) => {
-    e.preventDefault()
-    getToken(email, password)
+  const exchangeTokenForAuth = async (token) => {
+    const response = await axios.get(`${url}/auth/session`, headers(token))
+    setAuth(response.data)
   }
 
-  return (
-    <div className="App">
+  const register = async (credentials) => {
+    const headers = {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-robot-art-api-key': process.env.REACT_APP_API_KEY,
+      },
+    }
+    const { email, password } = credentials
+    await axios
+      .post(`${url}/auth/register`, credentials, headers)
+      .then((response) => {
+        login({ email, password })
+      })
+      .catch((error) => {
+        setRegisterError(error.response.statusText)
+      })
+  }
+
+  if (!auth) {
+    return (
       <div>
-        <form onSubmit={onSubmit}>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit">Submit</button>
-        </form>
+        <h1>Login</h1>
+        <Login login={login} loginError={loginError} setLoginError={setLoginError} />
+        <hr />
+        <h1>Register</h1>
+        <Register register={register} registerError={registerError} setRegisterError={setRegisterError} />
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div>
+        <h1>Welcome {auth.name}</h1>
+      </div>
+    )
+  }
 }
 
 export default App
---
+++ src/components/Login.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+
+const Login = ({ getToken, loginError, setLoginError }) => {
+  const [email, setEmail] = useState('admin@mondorobot.com')
+  const [password, setPassword] = useState('R0bot4Lif3')
+
+  useEffect(() => {
+    setLoginError('')
+  }, [email, password, setLoginError])
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    getToken(false, { email, password })
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+        {loginError && <p>{loginError}</p>}
+      </form>
+    </div>
+  )
+}
+
+export default Login
