1bd49ff2986d8fd122819f347caf68b6364fe964
Nick Oelsner <noelsner@gmail.com>
2021-08-17 12:26:42 -0700

    style nav for desktop and mobile

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,78 @@
-import { Link } from 'react-router-dom'
+import { useRef } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import '../styles/nav.scss'
 
 const Nav = ({ logOut, isAdmin }) => {
+  // const [onCurrentPage, setOnCurrentPage] = useState(false)
+  const navMenu = useRef()
+  const hamburger = useRef()
+  const header = useRef()
+
+  const location = useLocation()
+  const onCurrentPage = (pathName) => (location.pathname.replace('/app/', '') === pathName ? 'current' : '')
+
+  console.log('location :>> ', location.pathname.replace('/app/', ''))
+  console.log('onCurrentPage :>> ', onCurrentPage('robots'))
+
+  const handleHamburgerClick = () => {
+    navMenu.current.classList.toggle('active')
+    hamburger.current.classList.toggle('active')
+    header.current.classList.toggle('active')
+  }
+
+  const closeMenu = () => {
+    navMenu.current.classList.remove('active')
+    hamburger.current.classList.remove('active')
+    header.current.classList.remove('active')
+  }
+
   return (
-    <div>
-      <Link to="/app/robots">Robots</Link>
-      <Link to="/app/results">Results</Link>
-      {isAdmin ? <Link to="/app/admin">Admin</Link> : null}
-      <button onClick={logOut}>Logout</button>
-    </div>
+    <header className="header" ref={header}>
+      <nav className="navbar">
+        <Link to="/app/robots" className="nav-logo">
+          <img src="/assets/MondoRobotLogo.png" alt="Mondo Robot Logo" />
+        </Link>
+        <div className="nav-menu" ref={navMenu}>
+          <ul>
+            <li>
+              <Link to="/app/robots" className={`${onCurrentPage('robots')}`} onClick={closeMenu}>
+                Robots
+              </Link>
+            </li>
+            <li>
+              <Link to="/app/results" className={`${onCurrentPage('results')}`} onClick={closeMenu}>
+                Results
+              </Link>
+            </li>
+          </ul>
+          <ul>
+            {isAdmin ? (
+              <li className="secondary-link">
+                <Link to="/app/admin" className={`${onCurrentPage('admin')}`} onClick={closeMenu}>
+                  Admin
+                </Link>
+              </li>
+            ) : null}
+            <li className="secondary-link">
+              <Link
+                to="/"
+                onClick={() => {
+                  closeMenu()
+                  logOut()
+                }}
+              >
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <button className="hamburger" ref={hamburger} onClick={handleHamburgerClick}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </button>
+      </nav>
+    </header>
   )
 }
 
